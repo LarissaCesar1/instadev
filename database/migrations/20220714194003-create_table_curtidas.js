53e71038bf141838d74f2c53fe0f3c93033c4aef
Larissa Cesar <larissavitoriacesar1415@gmail.com>
2022-07-14 17:35:50 -0300

    Criando todas as migrates

---
+++ database/migrations/20220714194003-create_table_curtidas.js
@@ -0,0 +1,28 @@
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('curtidas', 
+   { 
+    id:{
+      type: Sequelize.DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    usuarios_id:{
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+    },
+    publicacoes_id:{
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+    },
+    
+  });
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('curtidas');
+     
+  }
+};
---
+++ database/migrations/20220714200859-create_table_amizades.js
@@ -0,0 +1,31 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('amizades',
+      {
+        id: {
+          type: Sequelize.DataTypes.INTEGER,
+          autoIncrement: true,
+          allowNull: false,
+          primaryKey: true,
+        },
+        usuarios_id1: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        usuarios_id2: {
+          type: Sequelize.DataTypes.INTEGER,
+          allowNull: false,
+        },
+        aceita: {
+          type: Sequelize.DataTypes.BOOLEAN,
+          allowNull: false,
+        },
+      }
+    )
+  },
+
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('amizades')
+  },
+};
