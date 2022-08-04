5f24bfb02bcb9e07a9a9170cc42d0871b2c8f33e
Larissa Cesar <larissavitoriacesar1415@gmail.com>
2022-08-04 18:30:07 -0300

    Inseridas informaçoes BD

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
 const UsuariosRouter = require('./routers/UsuariosRouter');
+
+app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/', UsuariosRouter);
 app.listen(3000,()=>{console.log("Estou escutando na porta 3000")})
--- routers/UsuariosRouter.js
+++ routers/UsuariosRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const UsuariosController = require('../controllers/UsuariosController');
 const router = express.Router();
 router.get('/create', UsuariosController.create);
-
+router.post('/create', UsuariosController.store);
 
 
 module.exports = router
